286067bc8f3b953f932133e3ef1eb36af476c686
Rubendiaro <145030175+Rubendiaro@users.noreply.github.com>
2025-02-27 12:17:23 +0100

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+/*
+============================================================
+  Generando a base de datos y los esquemas
+============================================================
+*/
+
+-- creando la base de datos DataWarehouse
+
+create database datawarehouse;
+
+-- creando el esquema
+
+create schema bronze;
+
+create schema silver;
+
+create schema gold;
+
